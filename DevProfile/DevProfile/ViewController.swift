2a583630097bd2b83bd898ae1c28f5e316a02f5f
Quinton Quaye <quintonquaye@Quintons-MacBook-Pro.local>
2017-10-17 21:13:23 -0500

    innitial xcode project

---
+++ DevProfile/DevProfile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DevProfile
+//
+//  Created by Quinton Quaye on 10/17/17.
+//  Copyright © 2017 Quinton Quaye. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
